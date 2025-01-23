b87dfdae5a0d2fc5c0e00dc186fc35a6aa49ac5b
manju-sakthi <sakthimanju098@gmail.com>
2025-01-23 09:47:30 +0530

    first commit

---
+++ Review1/src/main/java/package21/Method.java
@@ -0,0 +1,21 @@
+package package21;
+
+public class Method {
+
+	public static void main(String[] args) {
+		
+		Method D = new Method();
+		D.Display();
+		D.Get(1,"Anoop");
+		// TODO Auto-generated method stub
+		
+		}
+		public void Display() {
+		System.out.println("Display");
+			
+		}
+		public void Get(int a, String b) {
+			System.out.println("The value and Name is "+a +" "+b);
+	}
+
+}
